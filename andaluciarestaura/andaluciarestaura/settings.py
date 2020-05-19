bca105f553976834b4e77dea3e6638b3af74ca20
Charlowit <cargalvezp@gmail.com>
2020-05-19 12:22:38 +0200

    Models.py, settings.py

--- andaluciarestaura/andaluciarestaura/settings.py
+++ andaluciarestaura/andaluciarestaura/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$s=6@kmmhg$yxavb)m*#n-72qk6*gr+s7bnwh@#voxod&ef^p%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','54.14.184.104']
+ALLOWED_HOSTS = ['localhost','172.31.16.134']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'andaluciarestaura.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- andaluciarestaura/backend/models.py
+++ andaluciarestaura/backend/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser 
 # Create your models here.
+
+class Negocio(AbstractUser):
+    cif = models.CharField(max_length=100)
+    razon_social = models.CharField(max_length=100)
+    marca_comercial = models.CharField(max_length=100)
+    nombre_fiscal = models.CharField(max_length=100)
+    tipo_negocio = models.CharField(max_length=100)
+    tipo_via = models.CharField(max_length=100)
+    direccion_fiscal = models.CharField(max_length=100)
+    localidad = models.CharField(max_length=100)
+    codigo_postal = models.IntegerField()
+    correo_electronico = models.EmailField(max_length=100)
+    telefono_1 = models.IntegerField()
+    telefono_2 = models.IntegerField()
+    fax = models.CharField(max_length=100)
+    iban = models.CharField(max_length=100)
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return " {}".format(self.cif)
