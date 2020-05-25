d595cb045313bb61a1e64d477556623dbe4974b4
Charlowit <cargalvezp@gmail.com>
2020-05-25 21:18:24 +0200

    Merge

--- andaluciarestaura/accounts/serializers.py
+++ andaluciarestaura/accounts/serializers.py
@@ -14,7 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
             model = User
-            fields = ('id', 'cif', 'password')
+            fields = ('id', 'cif', 'password', 'provincia', 'localidad', 'razon_social', 'marca_comercial', 'telefono_1', 'tipo_negocio', 'tipo_via', 'email' )
             extra_kwargs = {'password': {'write_only':True} }
 
     def create(self, validated_data):
