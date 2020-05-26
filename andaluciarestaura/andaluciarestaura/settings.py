e94c5581733ef8e010323570c81bc608f60c53bf
Ubuntu <ubuntu@ip-172-31-16-134.us-east-2.compute.internal>
2020-05-26 09:26:36 +0000

    Proyecto limpio y configurado

--- andaluciarestaura/andaluciarestaura/settings.py
+++ andaluciarestaura/andaluciarestaura/settings.py
@@ -82,26 +82,26 @@ WSGI_APPLICATION = 'andaluciarestaura.wsgi.application'
 # Database PRODUCCION
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'andaluciarestaura',
-#        'USER': 'andaluciarestaura',
-#        'PASSWORD': 'jzif0039',
-#        'HOST': 'localhost',
-#        'PORT': '',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'andaluciarestaura',
+        'USER': 'andaluciarestaura',
+        'PASSWORD': 'jzif0039',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Database DEV
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -150,4 +150,10 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
 
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 PUBLIC_URL = ''
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = '/home/ubuntu/andaluciarestaura/andaluciarestaura/frontend/static'
