c60e377381723ceb70b1c36864749786134046a5
MateoJCR <mateojcr@gmail.com>
2020-05-27 09:48:41 +0200

    Subida

--- andaluciarestaura/carta/views.py
+++ andaluciarestaura/carta/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
 # Create your views here.
+def react(response,cif):
+    return HttpResponse("You're looking at question %s." % cif)
+
+def index(request,id_carta):
+    template = loader.get_template('carta/test.html')
+    context = {
+        'id_carta': id_carta,
+    }
+    return HttpResponse(template.render(context, request))
