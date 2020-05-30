a05caa65e0aaa309ac89a195020dc10b5c9c9f55
Charlowit <cargalvezp@gmail.com>
2020-05-30 16:42:26 +0200

    premium y alergenos

--- andaluciarestaura/carta/serializers.py
+++ andaluciarestaura/carta/serializers.py
@@ -7,7 +7,7 @@ class ProductosSerializer(serializers.HyperlinkedModelSerializer):
     category_name = serializers.CharField(source='categoria.name')
     class Meta:
         model = Productos
-        fields = ('carta_id','name','descripcion','id','tamanio','precio1','precio2','precio3','category_name')
+        fields = ('carta_id','name','descripcion','id','tamanio','precio1','precio2','precio3','category_name', 'is_apio', 'is_altramuces', 'is_cacahuete', 'is_crustaceo', 'is_frutos_con_cascara', 'is_gluten', 'is_huevo', 'is_lacteo', 'is_molusco', 'is_mostaza', 'is_pescado', 'is_sesamo', 'is_soja')
     def create(self, validated_data):
         subject = Productos.objects.create(carta=validated_data['carta']['id'], child_name=validated_data['name'])
         return subject
