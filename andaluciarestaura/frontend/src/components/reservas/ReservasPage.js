e3c1fac55b1981fdcf44ebe2a1dd8e893c5ff380
unknown <alejandrona5b@gmail.com>
2020-05-30 17:03:17 +0200

    Boton reservas arreglado

--- andaluciarestaura/frontend/src/components/reservas/ReservasPage.js
+++ andaluciarestaura/frontend/src/components/reservas/ReservasPage.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 const div1 = {
-    width:'75%'
+    width: '75%'
 };
 const div2 = {
     marginTop: '4%'
@@ -9,18 +9,17 @@ const div3 = {
     marginTop: '2%'
 };
 const div4 = {
-    textAlign:'right'
+    textAlign: 'right'
 };
 const div5 = {
     marginLeft: '0%',
     marginTop: '0%',
-    border: '2px solid orange',
-    width:'75%',
+    border: '4px solid orange',
+    width: '75%',
     heigth: '200px'
 };
 const div6 = {
-    border: '2px solid orange',
-    width:'100%',
+    width: '100%',
     heigth: '100%'
 };
 const div7 = {
@@ -39,16 +38,16 @@ const div10 = {
 };
 const div11 = {
     display: 'inline-block',
-     textAlign:'right'
+    textAlign: 'right',
+    height: '100%'
 };
 const div12 = {
-    marginTop:'7%',
-    marginLeft: '0%'
+    marginTop: '15px',
+    marginBottom: '5px'
 };
 const div13 = {
-    marginBottom:'10%',
-    marginLeft: '0%',
-    marginRight: '5%'
+
+
 };
 const div14 = {
     marginTop: '1%',
@@ -58,7 +57,7 @@ const div14 = {
 };
 const div15 = {
     width: '100%',
-    height:'100%'
+    height: '100%'
 };
 const div16 = {
     marginTop: '2%',
@@ -71,24 +70,24 @@ const div17 = {
 export class ReservasPage extends Component {
 
 
-    render(){
-        return(
+    render() {
+        return (
             <Fragment>
-                  <section class="section">
+                <section class="section">
                     <div class="container">
                         <div class="columns" >
-                          <div class="column">
-                            <h1 class="title">LISTADO DE RESERVAS</h1>
-                          </div>
-                          <div class="column">
-                            <div class="select">
-                              <select>
-                                <option>25/05/2020 - 26/05/2020</option>
-                                <option>26/05/2020 - 27/05/2020</option>
-                                <option>27/05/2020 - 28/05/2020</option>
-                              </select>
+                            <div class="column">
+                                <h1 class="title">LISTADO DE RESERVAS</h1>
                             </div>
-                          </div>
+                            <div class="column">
+                                <div class="select">
+                                    <select>
+                                        <option>25/05/2020 - 26/05/2020</option>
+                                        <option>26/05/2020 - 27/05/2020</option>
+                                        <option>27/05/2020 - 28/05/2020</option>
+                                    </select>
+                                </div>
+                            </div>
                         </div>
 
                         <hr style={div1} />
@@ -100,7 +99,7 @@ export class ReservasPage extends Component {
                                         <p> <b> Reserva 32487</b> </p>
                                         <div style={div3}>
                                             <span class="icon is-small is-left">
-                                              <i class="fas fa-user-circle">   </i>
+                                                <i class="fas fa-user-circle">   </i>
                                             </span>
                                             Alejandro Nieto
                                         </div>
@@ -108,11 +107,11 @@ export class ReservasPage extends Component {
                                     <div class="column is-three-quarters-mobile" style={div4}>
                                         <p> <b> Mesa </b> </p>
                                         <div class="select">
-                                          <select>
-                                            <option>T14</option>
-                                            <option>T24</option>
-                                            <option>T67</option>
-                                          </select>
+                                            <select>
+                                                <option>T14</option>
+                                                <option>T24</option>
+                                                <option>T67</option>
+                                            </select>
                                         </div>
                                     </div>
                                 </div>
@@ -120,7 +119,7 @@ export class ReservasPage extends Component {
                                 <div class="columns" style={div5}>
                                     <div style={div6}>
                                         <div class="column is-four-fifths is-three-quarters-mobile" style={div8}>
-                                            <div class="columns">
+                                            <div class="columns ">
                                                 <div class="column is-one-third is-four-fifths-mobile" style={div9}>
                                                     <div style={div7}>
                                                         <p style={div2}> <b> Fecha y hora </b></p>
@@ -129,21 +128,21 @@ export class ReservasPage extends Component {
 
                                                                 <div style={div10}>
                                                                     <span class="icon is-small is-left">
-                                                                      <i class="fas fa-calendar-alt">   </i>
+                                                                        <i class="fas fa-calendar-alt">   </i>
                                                                     </span>
                                                                     25/05/2020
                                                                 </div>
 
                                                                 <div style={div10}>
                                                                     <span class="icon is-small is-left">
-                                                                      <i class="fas fa-hourglass-start">   </i>
+                                                                        <i class="fas fa-hourglass-start">   </i>
                                                                     </span>
                                                                     19:00
                                                                 </div>
 
                                                                 <div style={div10}>
                                                                     <span class="icon is-small is-left">
-                                                                      <i class="fas fa-hourglass-end">   </i>
+                                                                        <i class="fas fa-hourglass-end">   </i>
                                                                     </span>
                                                                     22:00
                                                                 </div>
@@ -159,7 +158,7 @@ export class ReservasPage extends Component {
                                                             <div class="table-cell has-icons-left" style={div10}>
                                                                 <div>
                                                                     <span class="icon is-small is-left">
-                                                                      <i class="fas fa-user">   </i>
+                                                                        <i class="fas fa-user">   </i>
                                                                     </span>
                                                                     4
                                                                 </div>
@@ -173,7 +172,7 @@ export class ReservasPage extends Component {
                                                         <div style={div16}>
                                                             <div class="table-cell has-icons-left" style={div10}>
                                                                 <span class="icon is-small is-left">
-                                                                  <i class="fas fa-phone">   </i>
+                                                                    <i class="fas fa-phone">   </i>
                                                                 </span>
                                                                 661353289
                                                             </div>
@@ -182,43 +181,46 @@ export class ReservasPage extends Component {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="column is-three-quarters-mobile"  style={div11}>
-                                            <div >
-                                                <div>
+                                    </div>
+                                    <div class="column is-full-mobile" style={div11}>
+                                        <div class="has-text-centered">
+                                            <div>
                                                 <button class="button is-success" style={div12}>Confirmar reserva</button>
-                                                </div>
-                                                <div style={div7}>
-                                                <button class="button is-danger" style={div13}>Cancelar reserva</button>
-                                                </div>
+                                            </div>
+                                            <div style={div7}>
+                                                <button class="button is-danger is-marginless is-paddingless">
+                                                    <p style={{ paddingLeft: '10px', paddingRight: '10px' }}> Cancelar reserva </p> </button>
                                             </div>
                                         </div>
                                     </div>
+
                                 </div>
                             </div>
 
                         </div>
                         <hr style={div1} />
 
+
                         <div class="table" style={div2}>
                             <div class="table-row">
                                 <div class="columns" style={div1}>
                                     <div class="column is-four-fifths is-three-quarters-mobile">
-                                        <p> <b> Reserva 32187</b> </p>
+                                        <p> <b> Reserva 32487</b> </p>
                                         <div style={div3}>
                                             <span class="icon is-small is-left">
-                                              <i class="fas fa-user-circle">   </i>
+                                                <i class="fas fa-user-circle">   </i>
                                             </span>
-                                            Carlos Gálvez
+                                            Alejandro Nieto
                                         </div>
                                     </div>
                                     <div class="column is-three-quarters-mobile" style={div4}>
                                         <p> <b> Mesa </b> </p>
                                         <div class="select">
-                                          <select>
-                                            <option>T14</option>
-                                            <option>T24</option>
-                                            <option>T67</option>
-                                          </select>
+                                            <select>
+                                                <option>T14</option>
+                                                <option>T24</option>
+                                                <option>T67</option>
+                                            </select>
                                         </div>
                                     </div>
                                 </div>
@@ -226,7 +228,7 @@ export class ReservasPage extends Component {
                                 <div class="columns" style={div5}>
                                     <div style={div6}>
                                         <div class="column is-four-fifths is-three-quarters-mobile" style={div8}>
-                                            <div class="columns">
+                                            <div class="columns ">
                                                 <div class="column is-one-third is-four-fifths-mobile" style={div9}>
                                                     <div style={div7}>
                                                         <p style={div2}> <b> Fecha y hora </b></p>
@@ -235,23 +237,23 @@ export class ReservasPage extends Component {
 
                                                                 <div style={div10}>
                                                                     <span class="icon is-small is-left">
-                                                                      <i class="fas fa-calendar-alt">   </i>
+                                                                        <i class="fas fa-calendar-alt">   </i>
                                                                     </span>
-                                                                    30/05/2020
+                                                                    25/05/2020
                                                                 </div>
 
                                                                 <div style={div10}>
                                                                     <span class="icon is-small is-left">
-                                                                      <i class="fas fa-hourglass-start">   </i>
+                                                                        <i class="fas fa-hourglass-start">   </i>
                                                                     </span>
-                                                                    11:00
+                                                                    19:00
                                                                 </div>
 
                                                                 <div style={div10}>
                                                                     <span class="icon is-small is-left">
-                                                                      <i class="fas fa-hourglass-end">   </i>
+                                                                        <i class="fas fa-hourglass-end">   </i>
                                                                     </span>
-                                                                    15:00
+                                                                    22:00
                                                                 </div>
 
                                                             </div>
@@ -265,9 +267,9 @@ export class ReservasPage extends Component {
                                                             <div class="table-cell has-icons-left" style={div10}>
                                                                 <div>
                                                                     <span class="icon is-small is-left">
-                                                                      <i class="fas fa-user">   </i>
+                                                                        <i class="fas fa-user">   </i>
                                                                     </span>
-                                                                    8
+                                                                    4
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -279,32 +281,33 @@ export class ReservasPage extends Component {
                                                         <div style={div16}>
                                                             <div class="table-cell has-icons-left" style={div10}>
                                                                 <span class="icon is-small is-left">
-                                                                  <i class="fas fa-phone">   </i>
+                                                                    <i class="fas fa-phone">   </i>
                                                                 </span>
-                                                                696696969
+                                                                661353289
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="column is-three-quarters-mobile"  style={div11}>
-                                            <div >
-                                                <div>
+                                    </div>
+                                    <div class="column is-full-mobile" style={div11}>
+                                        <div class="has-text-centered">
+                                            <div>
                                                 <button class="button is-success" style={div12}>Confirmar reserva</button>
-                                                </div>
-                                                <div style={div7}>
-                                                <button class="button is-danger" style={div13}>Cancelar reserva</button>
-                                                </div>
+                                            </div>
+                                            <div style={div7}>
+                                                <button class="button is-danger is-marginless is-paddingless">
+                                                    <p style={{ paddingLeft: '10px', paddingRight: '10px' }}> Cancelar reserva </p> </button>
                                             </div>
                                         </div>
                                     </div>
+
                                 </div>
                             </div>
-
                         </div>
                     </div>
-                  </section>
+                </section>
             </Fragment>
         )
     }
